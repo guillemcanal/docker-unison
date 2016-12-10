a6c9ba52395000a31d20c8e6b657d5910cf228bb
Guillem CANAL <hello@guillem.me>
2016-12-10 13:43:02 +0100

    fix: Better support for Windows

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,19 @@
 # Check mount
 [ ! -d $UNISON_SOURCE ] && (echo "No directory mounted on $UNISON_SOURCE"; exit 1)
 
+UNISON_ARGS=""
+
 # If osxfs mount use unison fsmonitor
 # Else, synchronize every 2 seconds (Samba shares don't support inotify)
 if mount -t fuse.osxfs | grep $UNISON_SOURCE > /dev/null; then
 	UNISON_REPEAT="watch"
 else
 	UNISON_REPEAT="2"
+fi
+
+# Samba will mount a FAT filsystem which don't support all permission bits
+if mount -t cifs | grep $UNISON_SOURCE > /dev/null; then
+	UNISON_ARGS="-fat"
 fi
 
 # Configure Timezone
@@ -28,4 +35,4 @@ if [ ! -d /home/$UNISON_USER/.unison ];then
 	chown $UNISON_USER:$UNISON_GROUP -R $UNISON_DIR $UNISON_SOURCE
 fi
 
-exec su-exec $UNISON_USER:$UNISON_GROUP unison $UNISON_SOURCE $UNISON_DIR -auto -batch -repeat $UNISON_REPEAT -ignore 'Path .git'
+exec su-exec $UNISON_USER:$UNISON_GROUP unison $UNISON_SOURCE $UNISON_DIR -silent -auto -batch -repeat $UNISON_REPEAT -ignore 'Path .git' $UNISON_ARGS
